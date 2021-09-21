8dd1c6655d1f6b105726cddfdc586e3846621db0
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-21 12:54:13 -0400

    commit tree 50b848b406e050c98243beb7d7fec399be5c83a8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validate :click_bait_validator
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    
+    
+    private
+
+    def click_bait_validator
+        if title
+            unless title.match?(/won't believe|secret|top \d|guess/i)
+                errors.add(:title, "Must be clickbait.")
+            end
+        end
+    end
+
 end
